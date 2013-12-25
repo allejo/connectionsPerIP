70e057bd2759c822ba19cac1365069ef526bf9ac
allejo <allejo@me.com>
2013-12-24 17:06:59 -0800

    Rewrote plugin to use a map to be less memory intensive than looping through all the players on each join

--- connectionsPerIP.cpp
+++ connectionsPerIP.cpp
@@ -16,73 +16,86 @@ Connections Per IP
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <memory>
+
 #include "bzfsAPI.h"
 #include "plugin_utils.h"
 
-class connectionsPerIP : public bz_Plugin
+class ConnectionsPerIP : public bz_Plugin
 {
 public:
-    virtual const char* Name (){return "Connections Per IP";}
+    virtual const char* Name () {return "Connections Per IP";}
     virtual void Init (const char* config);
     virtual void Event (bz_EventData *eventData);
     virtual void Cleanup (void);
 
+    std::map <std::string, int> currentIPs;
     int maxConnectionsPerIP;
 };
 
-BZ_PLUGIN(connectionsPerIP)
+BZ_PLUGIN(ConnectionsPerIP)
 
-void connectionsPerIP::Init(const char* commandLine)
+void ConnectionsPerIP::Init (const char* commandLine)
 {
-    bz_debugMessage(4, "connectionsPerIP plugin loaded");
+    // Register our events with Register()
+    Register(bz_eAllowPlayer);
+    Register(bz_ePlayerJoinEvent);
+    Register(bz_ePlayerPartEvent);
 
-    if (commandLine != "" && atoi(commandLine))
+    // Check to see if the command line has a parameter so we can change
+    // the default amount of max connections. Make sure that the specified
+    // value is greater than or equal to one or else it would be silly
+    if (commandLine != "" && atoi(commandLine) >= 1)
     {
         maxConnectionsPerIP = atoi(commandLine);
     }
-    else
+    else // The user didn't specify an amount of connections so use the default
     {
         maxConnectionsPerIP = 2;
     }
-
-    Register(bz_eAllowPlayer);
 }
 
-void connectionsPerIP::Cleanup(void)
+void ConnectionsPerIP::Cleanup (void)
 {
-    bz_debugMessage(4, "connectionsPerIP plugin unloaded");
-
-    Flush();
+    Flush(); // Clean up all the events
 }
 
-void connectionsPerIP::Event(bz_EventData* eventData)
+void ConnectionsPerIP::Event(bz_EventData *eventData)
 {
     switch (eventData->eventType)
     {
-        case bz_eAllowPlayer:
+        case bz_eAllowPlayer: // This event is called each time a player connects to the server
         {
-            bz_AllowPlayerEventData_V1* allowPlayer = (bz_AllowPlayerEventData_V1*)eventData;
-            bz_APIIntList *playerlist = bz_newIntList();
-            bz_getPlayerIndexList(playerlist);
-            int conflicts = 0;
+            bz_AllowPlayerEventData_V1* allowPlayerData = (bz_AllowPlayerEventData_V1*)eventData;
 
-            for (int i = 0; i < playerlist->size(); i++)
+            if (currentIPs[allowPlayerData->ipAddress] >= maxConnectionsPerIP)
             {
-                if (allowPlayer->ipAddress == bz_getPlayerByIndex(playerlist->get(i))->ipAddress)
-                {
-                    conflicts++;
-
-                    if (conflicts > maxConnectionsPerIP)
-                    {
-                        allowPlayer->allow = false;
-                        allowPlayer->reason = "You have exceeded the maximum connections allowed to this server per IP address.";
-                        bz_deleteIntList(playerlist);
-                        return;
-                    }
-                }
+                allowPlayerData->allow = false;
+                allowPlayerData->reason = "You have exceeded the maximum connections allowed to this server per IP address.";
             }
+        }
+        break;
 
-            bz_deleteIntList(playerlist);
+        case bz_ePlayerJoinEvent: // This event is called each time a player joins the game
+        {
+            bz_PlayerJoinPartEventData_V1* joinData = (bz_PlayerJoinPartEventData_V1*)eventData;
+
+            if (currentIPs.find(joinData->record->ipAddress.c_str()) == grade_list.end())
+            {
+                currentIPs[joinData->record->ipAddress] = 1;
+            }
+            else
+            {
+                currentIPs[joinData->record->ipAddress]++;
+            }
+        }
+        break;
+
+        case bz_ePlayerPartEvent: // This event is called each time a player leaves a game
+        {
+            bz_PlayerJoinPartEventData_V1* partData = (bz_PlayerJoinPartEventData_V1*)eventData;
+
+            currentIPs[partData->record->ipAddress]--;
         }
         break;
 
